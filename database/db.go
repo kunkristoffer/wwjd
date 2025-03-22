0d6fe6cb1b2f1f59ad97e04d2d752b38e5071b32
Kristoffer <kunkristoffer@gmail.com>
2025-03-22 17:38:38 +0100

    installed libsql

--- database/db.go
+++ database/db.go
@@ -2,30 +2,42 @@ package database
 
 import (
 	"database/sql"
-	"log"
+	"fmt"
+	"os"
+	"time"
 
-	_ "modernc.org/sqlite"
+	_ "github.com/tursodatabase/libsql-client-go/libsql"
 )
 
 var DB *sql.DB
 
-func InitDB(path string) {
+func InitDB(path string) error {
 	var err error
-	DB, err = sql.Open("sqlite", path)
+	primaryUrl := os.Getenv("TURSO_URL")
+	authToken := os.Getenv("TURSO_key")
+
+	url := fmt.Sprintf(`libsql://%s.turso.io?authToken=%s`, primaryUrl, authToken)
+	DB, err = sql.Open("libsql", url)
 	if err != nil {
-		log.Fatalf("Failed to open SQLite DB: %v", err)
+		return fmt.Errorf("failed to open db %s: %v", url, err)
 	}
 
-	createTable := `
+	// Configure connection pool
+	DB.SetConnMaxIdleTime(9 * time.Second)
+
+	// Create test table
+	_, err = DB.Exec(`
 	CREATE TABLE IF NOT EXISTS prompts (
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
 		date_asked TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 		question TEXT NOT NULL,
 		reply TEXT NOT NULL,
 		votes INTEGER DEFAULT 0
-	);`
+	);`)
 
-	if _, err := DB.Exec(createTable); err != nil {
-		log.Fatalf("Failed to create prompts table: %v", err)
+	if err != nil {
+		return fmt.Errorf("error creating table: %v", err)
 	}
+
+	return nil
 }
